9be6be92655c767398f9339334dda7a19033590c
Jostein Tveit <josteitv@pvv.ntnu.no>
2017-10-25 00:48:22 +0200

    Fixed build script

--- build.sh
+++ build.sh
@@ -3,25 +3,26 @@
 APP_NAME=connectiq-talktimer
 
 SDK_URL="https://developer.garmin.com/downloads/connect-iq/sdks/connectiq-sdk-win-2.3.4.zip"
-SDK_FILE="sdk.zip"
-SDK_DIR="sdk"
-
-PEM_FILE="/tmp/developer_key.pem"
-DER_FILE="/tmp/developer_key.der"
-
-wget -O "${SDK_FILE}" "${SDK_URL}"
-unzip "${SDK_FILE}" "bin/*" -d "${SDK_DIR}"
-
-openssl genrsa -out "${PEM_FILE}" 4096
-openssl pkcs8 -topk8 -inform PEM -outform DER -in "${PEM_FILE}" -out "${DER_FILE}" -nocrypt
 
 PROJECT_HOME="${PWD}"
 RESOURCES_FOLDER="resources"
 SOURCE_FOLDER="source"
+BUILD_DIR=build
+SDK_FILE="${BUILD_DIR}/sdk.zip"
+SDK_DIR="${BUILD_DIR}/sdk"
+
+mkdir -p ${BUILD_DIR}
+
+wget -O "${SDK_FILE}" "${SDK_URL}"
+unzip "${SDK_FILE}" "bin/*" -d "${SDK_DIR}"
+
+openssl genrsa -out ${BUILD_DIR}/key.pem 4096
+openssl pkcs8 -topk8 -inform PEM -outform DER -in ${BUILD_DIR}/key.pem -out ${BUILD_DIR}/key.p8.der -nocrypt
+
 RESOURCES="`cd /; find \"${PROJECT_HOME}/${RESOURCES_FOLDER}\"* -iname '*.xml' | tr '\n' ':'`"
 SOURCES="`cd /; find \"${PROJECT_HOME}/${SOURCE_FOLDER}\" -iname '*.mc' | tr '\n' ' '`"
 
-"${SDK_DIR}/bin/monkeydo" "${PROJECT_HOME}/${APP_NAME}.prg" -t
+java -jar "${SDK_DIR}/bin/monkeybrains.jar" --package-app --manifest manifest.xml --output ${BUILD_DIR}/${APP_NAME}.iq --release --warn --private-key ${BUILD_DIR}/key.p8.der --rez ${RESOURCES} ${SOURCES}
 
-java -jar "${SDK_DIR}/bin/monkeybrains.jar" --package-app --manifest manifest.xml --output \"${APP_NAME}.iq\" --release --warn --private-key \"${DER_FILE}\" --rez \"${RESOURCES}\"
+#"${SDK_DIR}/bin/monkeydo" "${PROJECT_HOME}/${APP_NAME}.prg" -t
 
