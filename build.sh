3b0cb94cc5e78e8f31b88d7b1027f0376c178b1f
Jostein Tveit <josteitv@pvv.ntnu.no>
2018-10-16 22:11:01 +0200

    Added dos2unix in build script

--- build.sh
+++ build.sh
@@ -15,6 +15,7 @@ mkdir -p ${BUILD_DIR}
 
 wget -O "${SDK_FILE}" "${SDK_URL}"
 unzip "${SDK_FILE}" "bin/*" -d "${SDK_DIR}"
+dos2unix ${SDK_DIR}/bin/monkeyc
 chmod 755 ${SDK_DIR}/bin/monkeyc
 
 openssl genrsa -out ${BUILD_DIR}/key.pem 4096
